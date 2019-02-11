e27857c84df3b6df8e929dc60a233393db966e04
Jim O'Brien <jim.obrien3@gmail.com>
2019-02-11 09:09:34 -0500

    changed css and js to use wp_register_script so assets only load on the pages that shortcodes render on

--- pegasus-tabs.php
+++ pegasus-tabs.php
@@ -33,8 +33,8 @@ Domain Path: /languages
 	
 	
 	function pegasus_tabs_plugin_styles() {
-		
-		wp_enqueue_style( 'tabs-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/tabs.css', array(), null, 'all' );
+
+		wp_register_style( 'tabs-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/tabs.css', array(), null, 'all' );
 		//wp_enqueue_style( 'slippery-slider-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/slippery-slider.css', array(), null, 'all' );
 		
 	}
@@ -46,7 +46,7 @@ Domain Path: /languages
 	function pegasus_tabs_plugin_js() {
 		
 		//wp_enqueue_script( 'tabs-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/plugin.js', array( 'jquery' ), null, true );
-		wp_enqueue_script( 'pegasus-tabs-plugin-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/plugin.js', array( 'jquery' ), null, true );
+		wp_register_script( 'pegasus-tabs-plugin-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/plugin.js', array( 'jquery' ), null, 'all' );
 		
 	} //end function
 	add_action( 'wp_enqueue_scripts', 'pegasus_tabs_plugin_js' );
@@ -56,46 +56,51 @@ Domain Path: /languages
 	*/
 	
 	if ( ! class_exists( 'TabsClass' ) ) {
-	class TabsClass {
+		class TabsClass {
 
-		protected $_tabs_divs;
+			protected $_tabs_divs;
 
-		public function __construct($tabs_divs = '') {
-			$this->_tabs_divs = $tabs_divs;
-			add_shortcode( 'tabs', array( $this, 'tabs_wrap') );
-			add_shortcode( 'tab', array( $this,'tab_block') );
+			public function __construct($tabs_divs = '') {
+				$this->_tabs_divs = $tabs_divs;
+				add_shortcode( 'tabs', array( $this, 'tabs_wrap') );
+				add_shortcode( 'tab', array( $this,'tab_block') );
+			}
+
+			function tabs_wrap ( $args, $content = null ) {
+				$output = '<div class="js-tab-widget"><ul class="tab-list" >' . do_shortcode($content) . '</ul>';
+
+				//$output .= '<div class="pegasus-tabs-content">';
+				$output .= $this->_tabs_divs;
+				//$output .= '</div>'; //end tabs content
+				$output .= '</div>'; //end pegasus-tabs
+
+
+				wp_enqueue_style( 'tabs-css' );
+				wp_enqueue_script( 'pegasus-tabs-plugin-js' );
+
+				return $output;
+			}
+
+			function tab_block( $args, $content = null ) {
+				extract(shortcode_atts(array(
+					'id' => '',
+					'title' => '',
+					'class' => '',
+				), $args));
+
+				$output = '
+					<li class="tab-item ' . $class . '">
+						<a class="tab-link" href="#tab-' . $id . '" >' . $title . '</a>
+					</li>
+				';
+
+				$this->_tabs_divs.= '<section id="tab-' . $id . '" class="tab-panel">' . $content . '</section>';
+
+				return $output;
+			}
+
 		}
-
-		function tabs_wrap ( $args, $content = null ) {
-			$output = '<div class="js-tab-widget"><ul class="tab-list" >' . do_shortcode($content) . '</ul>';
-			
-			//$output .= '<div class="octane-tabs-content">';
-			$output .= $this->_tabs_divs;
-			//$output .= '</div>'; //end tabs content
-			$output .= '</div>'; //end octane-tabs
-		   return $output;
-		}
-
-		function tab_block( $args, $content = null ) {
-			extract(shortcode_atts(array(  
-				'id' => '',
-				'title' => '', 
-				'class' => '', 
-			), $args));
-
-			$output = '
-				<li class="tab-item '.$class.'">
-					<a class="tab-link" href="#tab-'.$id.'" >'.$title.'</a>
-				</li>
-			';
-
-			$this->_tabs_divs.= '<section id="tab-'.$id.'" class="tab-panel">' .$content. '</section>';
-
-			return $output;
-		}
-
-	}
-	new TabsClass;
+		new TabsClass;
 	}
 	
 	
