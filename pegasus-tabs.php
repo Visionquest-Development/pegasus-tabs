ab0940b038c47d8aed3f130db0fb34c20c0cc656
Jim O'Brien <jim.obrien3@gmail.com>
2024-12-11 00:51:49 -0500

    updating pegasus-tabs plugin: usage page when not pegasus theme

--- pegasus-tabs.php
+++ pegasus-tabs.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Plugin Name: Pegasus Tabs Plugin
 Plugin URI:	 https://developer.wordpress.org/plugins/the-basics/
@@ -12,49 +12,134 @@ Text Domain: wporg
 Domain Path: /languages
 */
 
-	global $wpdb; 
-	
-	add_action("admin_menu", "pegasus_tabs_menu_item");
-	function pegasus_tabs_menu_item() {
-		//add_menu_page("Tabs", "Tabs", "manage_options", "pegasus_tabs_plugin_options", "pegasus_tabs_plugin_settings_page", null, 99);
+	global $wpdb;
+
+	function pegasus_tabs_check_main_theme_name() {
+		$current_theme_slug = get_option('stylesheet'); // Slug of the current theme (child theme if used)
+		$parent_theme_slug = get_option('template');    // Slug of the parent theme (if a child theme is used)
+
+		//error_log( "current theme slug: " . $current_theme_slug );
+		//error_log( "parent theme slug: " . $parent_theme_slug );
+
+		if ( $current_theme_slug == 'pegasus' ) {
+			return 'Pegasus';
+		} elseif ( $current_theme_slug == 'pegasus-child' ) {
+			return 'Pegasus Child';
+		} else {
+			return 'Not Pegasus';
+		}
 	}
-	
-	
-	
+
+	function pegasus_tabs_menu_item() {
+		if ( pegasus_tabs_check_main_theme_name() == 'Pegasus' || pegasus_tabs_check_main_theme_name() == 'Pegasus Child' ) {
+			//do nothing
+		} else {
+			//echo 'This is NOT the Pegasus theme';
+			add_menu_page(
+				"Tabs", // Page title
+				"Tabs", // Menu title
+				"manage_options", // Capability
+				"pegasus_tabs_plugin_options", // Menu slug
+				"pegasus_tabs_plugin_settings_page", // Callback function
+				null, // Icon
+				93 // Position in menu
+			);
+		}
+	}
+	add_action("admin_menu", "pegasus_tabs_menu_item");
+
 	function pegasus_tabs_plugin_settings_page() { ?>
-		<div class="wrap pegasus-wrap">
-		<h1>Tabs</h1>
-		
-		<p>Usage: <pre>[tabs][tab class="first" title="Home"]Vivamus suscipit tortor eget felis porttitor volutpat. [/tab][tab class="second" title="Profile"]Pellentesque in ipsum id orci porta dapibus. [/tab][/tabs]</pre> </p>
-		
+	    <div class="wrap pegasus-wrap">
+			<h1>pegasus_tabs Usage</h1>
+
+			<div>
+				<h3>pegasus_tabs Usage 1:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+
+					input[type="text"].code {
+						width: 100%;
+					}
+				</style>
+				<pre >[tabs]
+	[tab class="first" title="Home"]
+		Vivamus suscipit tortor eget felis porttitor volutpat.
+	[/tab]
+	[tab class="second" title="Profile"]
+		Pellentesque in ipsum id orci porta dapibus.
+	[/tab]
+[/tabs]</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[tabs][tab class="first" title="Home"]Vivamus suscipit tortor eget felis porttitor volutpat. [/tab][tab class="second" title="Profile"]Pellentesque in ipsum id orci porta dapibus.[/tab][/tabs]'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<p style="color:red;">MAKE SURE YOU DO NOT HAVE ANY RETURNS OR <?php echo htmlspecialchars('<br>'); ?>'s IN YOUR SHORTCODES, OTHERWISE IT WILL NOT WORK CORRECTLY</p>
+
 		</div>
 	<?php
 	}
-	
-	
+
+	//add_action("admin_menu", "pegasus_tabs_menu_item");
+	//function pegasus_tabs_menu_item() {
+		//add_menu_page("Tabs", "Tabs", "manage_options", "pegasus_tabs_plugin_options", "pegasus_tabs_plugin_settings_page", null, 99);
+	//}
+
+
+	/*
+	function pegasus_tabs_plugin_settings_page() { ?>
+		<div class="wrap pegasus-wrap">
+		<h1>Tabs</h1>
+
+		<p>Usage: <pre>[tabs][tab class="first" title="Home"]Vivamus suscipit tortor eget felis porttitor volutpat. [/tab][tab class="second" title="Profile"]Pellentesque in ipsum id orci porta dapibus. [/tab][/tabs]</pre> </p>
+
+		</div>
+	<?php
+	}
+	*/
+
 	function pegasus_tabs_plugin_styles() {
 
 		wp_register_style( 'tabs-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/tabs.css', array(), null, 'all' );
 		//wp_enqueue_style( 'slippery-slider-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/slippery-slider.css', array(), null, 'all' );
-		
+
 	}
 	add_action( 'wp_enqueue_scripts', 'pegasus_tabs_plugin_styles' );
-	
+
 	/**
-	* Proper way to enqueue JS 
+	* Proper way to enqueue JS
 	*/
 	function pegasus_tabs_plugin_js() {
-		
+
 		//wp_enqueue_script( 'tabs-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/plugin.js', array( 'jquery' ), null, true );
 		wp_register_script( 'pegasus-tabs-plugin-js', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'js/plugin.js', array( 'jquery' ), null, 'all' );
-		
+
 	} //end function
 	add_action( 'wp_enqueue_scripts', 'pegasus_tabs_plugin_js' );
-	
+
 	/**
 	* Tabs Short Code
 	*/
-	
+
 	if ( ! class_exists( 'TabsClass' ) ) {
 		class TabsClass {
 
@@ -108,6 +193,6 @@ Domain Path: /languages
 		}
 		new TabsClass;
 	}
-	
-	
+
+
 	?>
